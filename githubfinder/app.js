e24916cf557db6333be935efd055232157946b67
Sean <sean@Sens-MacBook-Pro.local>
2018-08-08 10:05:19 -0500

    Github Finder [Part 3] - Display The Profile

--- githubfinder/app.js
+++ githubfinder/app.js
@@ -1,5 +1,7 @@
 // Instantiate Github
 const github = new GitHub();
+// Instantiate UI
+const ui = new UI();
 
 // Search input
 const searchUser = document.getElementById('searchUser');
@@ -16,6 +18,7 @@ searchUser.addEventListener('keyup', e => {
         // Show alert
       } else {
         // Show profile
+        ui.showProfile(data.profile);
       }
     });
   } else {
